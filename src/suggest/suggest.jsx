9d60d97436cfede03b4684639a6eb385cdfe0ccd
Morgan Burnside <morganburn670@gmail.com>
2025-10-08 16:15:54 -0600

    convert suggest component

--- src/suggest/suggest.jsx
+++ src/suggest/suggest.jsx
@@ -2,8 +2,20 @@ import React from 'react';
 
 export function Suggest() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>suggest displayed here</div>
-    </main>
+        <main>
+            <h2>Song Suggestions</h2>
+            <h4>(Powered by iTunes API)</h4>
+            <form className="search-bar">
+                <label>Search song title or artist:</label>
+                <input type="text" placeholder="e.g. Huntrix Golden" />
+                <input type="button" value="Search" />
+            </form>
+
+            <div className="search-results">
+                <img src="images/album_cover.png" alt="Album Placeholder" width="60" />
+                <span className="song-text">Golden - Huntrix</span>
+                <button className="suggest-button">Suggest</button>
+            </div>
+        </main>
   );
 }
