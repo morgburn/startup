8ebc427186c8abafb0ef4063ef11c8eb24086b8b
Morgan Burnside <morganburn670@gmail.com>
2025-10-08 13:09:35 -0600

    Started .jsx files in page directories

---
+++ src/suggest/suggest.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Suggest() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>suggest displayed here</div>
+    </main>
+  );
+}
---
+++ src/vote/vote.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Vote() {
+  return (
+    <main className="container-fluid bg-secondary text-center">
+      <div>vote displayed here</div>
+    </main>
+  );
+}
