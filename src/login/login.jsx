8422b317a5231590de2d217ca897b87a1f821c9d
Morgan Burnside <morganburn670@gmail.com>
2025-10-08 16:12:28 -0600

    convert login component

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,22 @@ import React from 'react';
 
 export function Login() {
   return (
-    <main className="container-fluid bg-secondary text-center">
-      <div>login displayed here</div>
+    <main>
+      <form className="login-form" method="get" action="vote.html">
+        <h2>Login</h2>
+        <div>
+          <label>Name:</label>
+          <input type="text" placeholder="Your Name" />
+        </div>
+        <div>
+          <label>Password:</label>
+          <input type="text" placeholder="password" />
+        </div>
+        <div className="buttons">
+          <input type="button" value="Create" />
+          <input type="button" value="Login" />
+        </div>
+      </form>
     </main>
   );
 }
