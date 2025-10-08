6bb6f846b909e4005f337567ea532829ffbe5590
Morgan Burnside <morganburn670@gmail.com>
2025-10-08 13:06:25 -0600

    adding app.jsx with header and footer

--- src/app.jsx
+++ src/app.jsx
@@ -3,5 +3,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-  return <div className="body bg-dark text-light">App will display here</div>;
+  return (
+        <><header>
+          <h1>Bop Ballot</h1>
+          <nav>
+              <menu>
+                  <li><a href="index.html">Home</a></li>
+                  <li><a href="suggest.html">Make a Song Suggestion</a></li>
+                  <li><a href="vote.html">Vote</a></li>
+                  <li><a href="scores.html">View Top Suggestions</a></li>
+              </menu>
+          </nav>
+      </header>
+      
+      <main>App components go here</main>
+      
+      <footer>
+              <p>Morgan Burnside</p>
+              <a href="https://github.com/morgburn/startup">GitHub</a>
+          </footer></>
+  )
 }
