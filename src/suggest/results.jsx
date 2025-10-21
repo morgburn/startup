813825aca33ff0b7b23c0520c28d4e2ab23fbdb6
Morgan Burnside <morganburn670@gmail.com>
2025-10-21 17:15:29 -0600

    updated results.jsx

---
+++ src/suggest/results.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+export function SongResults({ songs }) {
+    if (songs.length === 0) {
+        return <p>No results</p>;
+    }
+
+    return (
+        <div className="search-results">
+            {songs.map((song, index) => (
+                <div key={index} className="result-item">
+                    <img src={song.albumCover} alt={song.name} width="60" />
+                    <span classname="song-text">
+                        {song.name} - (song.artist)
+                    </span>
+                    <button className="suggest-button">Suggest</button>
+                </div>
+            ))}
+        </div>
+    );
+}
