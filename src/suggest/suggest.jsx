7f68dd54fe8ef1a4cbb8855df8780a75f7ccd367
Morgan Burnside <morganburn670@gmail.com>
2025-10-21 17:24:26 -0600

    finished suggest.jsx

--- src/suggest/suggest.jsx
+++ src/suggest/suggest.jsx
@@ -13,30 +13,21 @@ export function Suggest() {
         ].filter(song => song.trackName.toLowerCase().includes(term.toLowerCase()));
     }
 
+    function handleSearch(term) {
+        if (!term) {
+            alert('Please enter a search term');
+            return;
+        }
+        const results = getSongs(term);
+        setSongs(results);
+    }
 
-
-
-
-
-
-
-
-
-  return (
+    return (
         <main>
             <h2>Song Suggestions</h2>
-            <h4>(Powered by iTunes API)</h4>
-            <form className="search-bar">
-                <label>Search song title or artist:</label>
-                <input type="text" placeholder="e.g. Huntrix Golden" />
-                <input type="button" value="Search" />
-            </form>
-
-            <div className="search-results">
-                <img src="images/album_cover.png" alt="Album Placeholder" width="60" />
-                <span className="song-text">Golden - Huntrix</span>
-                <button className="suggest-button">Suggest</button>
-            </div>
+            <h4>Powered by iTunes API</h4>
+            <SearchBar onSearch={handleSearch} />
+            <Results songs={songs} />
         </main>
-  );
+    );
 }
