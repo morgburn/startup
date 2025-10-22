4369c235e74ec7b21cd1dedd7d355db486b37937
Morgan Burnside <morganburn670@gmail.com>
2025-10-22 11:55:48 -0600

    Fixed voteItem.jsx for styling

--- src/vote/voteItem.jsx
+++ src/vote/voteItem.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 export function VoteItem({ song, isVoted, onVote }) {
     return (
         <div className="song-suggestion">
+            <img src="/images/album_cover.png" alt="Album Cover Placeholder" width="60" />
             <span className="song-text">
                 {song.trackName} - {song.artist}
             </span>
-            <button className={`heart-button ${isVoted ? 'voted' : ''}`} onClick={onVote}>❤️</button>
-            <span className="vote-count">{song.votes}</span>
+            <button className={`heart-button ${isVoted ? 'liked' : ''}`} onClick={onVote}>♥</button>
         </div>
     );
 }
