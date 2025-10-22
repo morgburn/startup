ae44052e4a551060d2d9ddfa77aa05a3f9fcd870
Morgan Burnside <morganburn670@gmail.com>
2025-10-22 11:10:42 -0600

    implemented voteList.jsx

--- src/vote/voteList.jsx
+++ src/vote/voteList.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
 import { voteItem } from './voteItem';
+
+export function voteList({ songs, voted, onVote }) {
+    return (
+        <div className="vote-list">
+            {songs.map(song => (
+                <voteItem key={song.id} song={song} isVoted={voted.includes(song.id)} onVote={() => onVote(song.id)} />
+            ))}
+        </div>
+    );
+}
