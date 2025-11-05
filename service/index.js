3b73494d660cd4a30bcb4f332fae28970e2f270f
Morgan Burnside <morganburn670@gmail.com>
2025-11-05 12:55:53 -0700

    updateScores added to index.js

--- service/index.js
+++ service/index.js
@@ -71,3 +71,25 @@ apiRouter.post('/score', verifyAuth, (req, res) => {
   scores = updateScores(req.body);
   res.send(scores);
 });
+
+// updateScores, keep only the top 10
+function updateScores(newScore) {
+  let found = false;
+  for (const [i, prevScore] of scores.entries()) {
+    if (newScore.score > prevScore.score) {
+      scores.splice(i, 0, newScore);
+      found = true;
+      break;
+    }
+  }
+
+  if (!found) {
+    scores.push(newScore);
+  }
+
+  if (scores.length > 10) {
+    scores.length = 10;
+  }
+
+  return scores;
+}
