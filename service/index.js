35783c184d220341127d6a32ffd0cfd2a530f804
Morgan Burnside <morganburn670@gmail.com>
2025-11-05 12:40:18 -0700

    register and login user api in index.js

--- service/index.js
+++ service/index.js
@@ -15,3 +15,28 @@ let users = [];
 let songs = [];
 let scores = [];
 
+var apiRouter = express.Router();
+app.use(`/api`, apiRouter);
+
+// CreateAuth - register a new user
+apiRouter.post('/auth/create', async (req, res) => {
+  if (await findUser('email', req.body.email)) {
+    res.status(409).send({ msg: 'Existing user' });
+  } else {
+    const user = await createUser(req.body.email, req.body.password);
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  }
+});
+
+// GetAuth - log in existing user
+apiRouter.post('/auth/login', async (req, res) => {
+  const user = await findUser('email', req.body.email);
+  if (user && (await bcrypt.compare(req.body.password, user.password))) {
+    user.token = uuid.v4();
+    setAuthCookie(res, user.token);
+    res.send({ email: user.email });
+  } else {
+    res.status(401).send({ msg: 'Unauthorized' });
+  }
+});
