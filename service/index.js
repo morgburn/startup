718c9fdd082018a2af2e98e6af58353a082019e0
Morgan Burnside <morganburn670@gmail.com>
2025-11-05 12:35:10 -0700

    Redo first part of index.js

--- service/index.js
+++ service/index.js
@@ -5,25 +5,13 @@ const uuid = require('uuid');
 const path = require('path');
 
 const app = express();
-const port = 4000;
+const port = process.argv.length > 2 ? process.argv[2] : 4000;
+const authCookieName = 'token';
 
 app.use(express.json());
 app.use(cookieParser());
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
-
 let users = [];
-let suggestions = [];
-const authCookieName = 'authToken';
-
-app.post('/api/auth/create', async (req, res) => {
-  if (users.find(u => u.email === req.body.email)) return res.status(409).send({ msg: 'Existing user' });
-
-  const passwordHash = await bcrypt.hash(req.body.password, 10);
-  const user = { email: req.body.email, password: passwordHash, token: uuid.v4() };
-  users.push(user);
-
-  res.cookie(authCookieName, user.token, { httpOnly: true, sameSite: 'strict', secure: true });
-  res.send({ email: user.email });
-});
+let songs = [];
+let scores = [];
 
