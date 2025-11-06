df366c09529afc27ecb9474a612ede12afa3b64b
Morgan Burnside <morganburn670@gmail.com>
2025-11-05 18:09:31 -0700

    Adding logout info to authenticated.jsx and login.jsx

--- src/login/authenticated.jsx
+++ src/login/authenticated.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 
 export function Authenticated({userName}) {
+  async function handleLogout() {
+    try {
+      await fetch('/api/auth/logout', {
+        method: 'DELETE',
+      });
+    } catch (err) {
+      console.error('Logout failed:', err);
+    } finally {
+      localStorage.removeItem('userName');
+      onLogout();
+    }
+  }
+
 
     return (
         <main className="welcome">
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -10,10 +10,15 @@ export function Login() {
     localStorage.setItem('userName', newUserName);
   }
 
+  function handleLogout() {
+    setUserName(null);
+    localStorage.removeItem('userName');
+  }
+
   return (
     <main>
       {userName ? (
-        <Authenticated userName={userName} />
+        <Authenticated userName={userName} onLogout={handleLogout} />
       ) : (
         <Unauthenticated onLogin={handleLogin} />
       )}
